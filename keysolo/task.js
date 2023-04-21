c331f30aa757a810248c2f31d11ef03ea577ef88
YuriyGl86 <yuriygl86@gmail.com>
2023-04-21 07:09:29 +0300

    keysolo

---
+++ keysolo/task.js
@@ -0,0 +1,120 @@
+class Game {
+  constructor(container) {
+    this.container = container;
+    this.wordElement = container.querySelector('.word');
+    this.winsElement = container.querySelector('.status__wins');
+    this.lossElement = container.querySelector('.status__loss');
+    this.timeElement = container.querySelector('.status__time')
+
+    this.reset();
+
+    this.registerEvents();
+  }
+
+  reset() {
+    this.setNewWord();
+    this.winsElement.textContent = 0;
+    this.lossElement.textContent = 0;
+  }
+
+  registerEvents() {
+    document.addEventListener('keyup',this.eventHandler.bind(this));
+    /*
+      TODO:
+      Написать обработчик события, который откликается
+      на каждый введённый символ.
+      В случае правильного ввода слова вызываем this.success()
+      При неправильном вводе символа - this.fail();
+      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
+     */
+  }
+
+  eventHandler(event){
+      console.log(event.key.charCodeAt(0), this.currentSymbol.textContent.charCodeAt(0));
+      console.log(event.key);
+
+      if(event.key == 'Shift' || event.key == 'Alt' || event.key == 'Control') return
+      if(event.key.charCodeAt(0) == this.currentSymbol.textContent.charCodeAt(0)) this.success();
+      else this.fail();
+  }
+
+
+  success() {
+    if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
+    this.currentSymbol.classList.add('symbol_correct');
+    this.currentSymbol = this.currentSymbol.nextElementSibling;
+
+    if (this.currentSymbol !== null) {
+      this.currentSymbol.classList.add('symbol_current');
+      return;
+    }
+
+    if (++this.winsElement.textContent === 10) {
+      alert('Победа!');
+      this.reset();
+    }
+    this.setNewWord();
+  }
+
+  fail() {
+    if (++this.lossElement.textContent === 5) {
+      alert('Вы проиграли!');
+      this.reset();
+    }
+    this.setNewWord();
+  }
+
+  setNewWord() {
+    const word = this.getWord();
+
+    this.renderWord(word);
+    
+    if(this.timer) clearInterval(this.timer);
+    this.timeElement.innerText = word.length;
+    this.timer = setInterval(this.timeCounter.bind(this), 1000)
+  }
+
+  timeCounter(){
+    if(--this.timeElement.innerText == 0){
+      this.fail();
+    }
+  }
+
+
+  getWord() {
+    const words = [
+        'bob',
+        'awesome',
+        'netology',
+        'hello',
+        'kitty',
+        'rock',
+        'youtube',
+        'popcorn',
+        'cinema',
+        'love',
+        'javascript',
+        'hello мир',
+        'hello Мир',
+        'я люблю kitkat'
+      ],
+      index = Math.floor(Math.random() * words.length);
+
+    return words[index];
+  }
+
+  renderWord(word) {
+    const html = [...word]
+      .map(
+        (s, i) =>
+          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+      )
+      .join('');
+    this.wordElement.innerHTML = html;
+
+    this.currentSymbol = this.wordElement.querySelector('.symbol_current');
+  }
+}
+
+new Game(document.getElementById('game'))
+
