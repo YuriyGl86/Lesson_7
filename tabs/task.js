73e021e1660120c0b7fb8f30c86038786afa2418
Yuriy Glushchenko <yuriygl86@gmail.com>
2023-04-21 15:20:14 +0300

    Tabs1

--- tabs/task.js
+++ tabs/task.js
@@ -1,13 +1,34 @@
-const tabList = Array.from(document.querySelectorAll('.tab'))
-const pageList = Array.from(document.querySelectorAll('.tab__content'))
-console.log(tabList)
 
-document.querySelector('.tab__navigation').addEventListener('click', tabHandler)
+class Tab{
 
-function tabHandler(event){
-    let tab = event.target.closest('.tab')
-    if(!tab) return
-    console.log(tab)
+    constructor(elem){
+    this.elem = elem
+    this.tabList = Array.from(elem.querySelectorAll('.tab'))
+    this.pageList = Array.from(elem.querySelectorAll('.tab__content'))
 
-    
+    elem.querySelector('.tab__navigation').addEventListener('click', this.tabHandler.bind(this))
+    }
+
+    tabHandler(event){
+        let tab = event.target.closest('.tab')
+        if(!tab) return
+       
+        let tabindex = this.tabList.findIndex(item => item == tab)
+        console.log(tabindex)
+        this.chengeTab(tabindex)
+        
+    }
+
+    chengeTab(index){
+        let oldTab = this.elem.querySelector('.tab_active')
+        let oldPage = this.elem.querySelector('.tab__content_active')
+
+        oldTab.classList.toggle('tab_active')
+        oldPage.classList.toggle('tab__content_active')
+
+        this.tabList[index].classList.toggle('tab_active')
+        this.pageList[index].classList.toggle('tab__content_active')
+    }
 }
+
+new Tab(document.querySelector('.tabs'))
